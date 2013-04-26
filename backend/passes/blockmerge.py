666a41d9b27c19d3454a3121db17a0c573ce683d
andrew <andrewchamberss@gmail.com>
2013-04-26 18:36:35 +1200

    fixed a bug in liveness analysis

--- backend/passes/blockmerge.py
+++ backend/passes/blockmerge.py
@@ -10,15 +10,41 @@ class BlockMerge(functionpass.FunctionPass):
             modified = False
             incoming = {}
             for b in f:
-                for s in b[-1].getSuccessors():
+                for s in b.getSuccessors():
                     if s not in incoming:
                         incoming[s] = []
                     incoming[s].append(b)
+            
+            #dict to update phi nodes
+            mappings = {}
             
             for b in incoming:
                 if len(incoming[b]) == 1:
                     pred = incoming[b][0]
                     if type(pred[-1]) == ir.Jmp:
+                        #combine the two...
+                        #combine the opcodes
                         pred.opcodes = pred.opcodes[:-1] + b.opcodes
+                        mappings[b] = pred
                         modified = True
+            
+            
+            
+            if modified:
+                mappingsChanged = True
+                #if there was a chain of remappings then do resolve it
+                while mappingsChanged:
+                    mappingsChanged = False
+                    for k in mappings.keys():
+                        if mappings[k] in mappings.keys():
+                            mappings[k] = mappings[mappings[k]]
+                            mappingsChanged = True
+                #update all phi instructions
+                for b in f:
+                    for instr in b:
+                        if type(instr) == ir.Phi:
+                            for idx,phiblock in enumerate(instr.blocks):
+                                if phiblock in mappings:
+                                    instr.blocks[idx] = mappings[phiblock]
+            
             return modified
